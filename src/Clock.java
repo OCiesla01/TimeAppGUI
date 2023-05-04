2f2d4cc48b0250028e9aa0f2194fe34ec5ffc5d5
Oskar Ciesla <oskar.ciesla01@gmail.com>
2023-05-04 16:21:56 +0200

    "Added current date to Clock."

--- src/Clock.java
+++ src/Clock.java
@@ -6,11 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Clock extends JFrame implements ActionListener {
-    JLabel mainLabel, topLine;
+    JLabel mainLabel, topLine, dayLabel;
     JButton backToMain;
-//    Calendar calendar;
-    SimpleDateFormat timeFormat;
-    String time;
+    SimpleDateFormat timeFormat, dayFormat, dateFormat;
+    String time, day, date;
     Clock() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600, 400);
@@ -21,12 +20,25 @@ public class Clock extends JFrame implements ActionListener {
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
         time = timeFormat.format(Calendar.getInstance().getTime());
 
+        dayFormat = new SimpleDateFormat("EEEE");
+        day = dayFormat.format(Calendar.getInstance().getTime());
+
+        dateFormat = new SimpleDateFormat("dd MMMMM yyyy");
+        date = dateFormat.format(Calendar.getInstance().getTime());
+
+
+        dayLabel = new JLabel();
+        dayLabel.setBounds(110, 270, 400, 50);
+        dayLabel.setFont(new Font("Oswald", Font.PLAIN, 35));
+        dayLabel.setOpaque(true);
+        dayLabel.setText(day + ", " + date);
+
         mainLabel = new JLabel();
         mainLabel.setText(time);
         mainLabel.setHorizontalAlignment(JLabel.CENTER);
         mainLabel.setVerticalAlignment(JLabel.CENTER);
         mainLabel.setFont(new Font("MV Boli", Font.PLAIN, 50));
-        mainLabel.setBounds(45, 70, 485, 250);
+        mainLabel.setBounds(45, 50, 485, 200);
         mainLabel.setBackground(new Color(0xCDCDCD));
         mainLabel.setOpaque(true);
         mainLabel.setBorder(BorderFactory.createLineBorder(new Color(0xA8A8A8), 2));
@@ -41,6 +53,8 @@ public class Clock extends JFrame implements ActionListener {
         backToMain.setFocusable(false);
         backToMain.addActionListener(this);
 
+
+        this.add(dayLabel);
         this.add(backToMain);
         this.add(topLine);
         this.add(mainLabel);
